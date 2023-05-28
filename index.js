a84521b9ab4863ecfb6696756b47e55fd7a2e43b
rajkumar-cmd <raymarhossor@gmail.com>
2023-05-28 23:39:13 +0530

    backend-login

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express=require("express");
 const {connection} = require("./db");
 const {userRouter} = require("./routes/users.route")
-const {OEM_SpecsRouter} =require("./routes/OEM_Specs.route")
-const {marketplaceRouter}=require("./routes/marketplace.route")
 require("dotenv").config();
 const app=express();
 const cors=require("cors");
@@ -12,8 +10,7 @@ app.get("/",(req,res)=>{
     res.send("Home Page");
 })
 app.use("/user",userRouter);
-app.use("/OEM_Specs",OEM_SpecsRouter);
-app.use("/marketplace",marketplaceRouter);
+
 app.listen(process.env.port,async()=>{
     try{
         await connection;
