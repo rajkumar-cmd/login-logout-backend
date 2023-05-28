a310d8dd5c7b0d1e7b680e53bb47d96c355a7b3f
rajkumar-cmd <raymarhossor@gmail.com>
2023-05-29 01:07:01 +0530

    backend-login

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,73 +1,90 @@
-const express=require("express");
-const {UserModel}=require("../model/users.model");
-const bcrypt=require("bcrypt");
+const express = require("express");
+const { UserModel } = require("../model/users.model");
+const bcrypt = require("bcrypt");
 
-const userRouter=express.Router();
+const userRouter = express.Router();
 
-userRouter.post("/register",async(req,res)=>{
-    const{name,
+userRouter.post("/register", async (req, res) => {
+    const { name,
         email,
         password,
         securityQuestion,
-        securityAnswer}=req.body;
-    try{
-        bcrypt.hash(password,5,async(err,hash)=>{
-            if(err){
+        securityAnswer } = req.body;
+    try {
+        bcrypt.hash(password, 5, async (err, hash) => {
+            if (err) {
                 console.log(err);
-            }else{
-                const user=new UserModel({name,
+            } else {
+                const user = new UserModel({
+                    name,
                     email,
-                    password:hash,
+                    password: hash,
                     securityQuestion,
-                    securityAnswer});
+                    securityAnswer
+                });
                 await user.save();
-                res.send({"msg":"Registered"})
+                res.send({ "msg": "Registered" })
             }
         })
-    }catch(err){
-        res.send({"msg":err})
+    } catch (err) {
+        res.send({ "msg": err })
     }
 })
 
-userRouter.post("/login",async(req,res)=>{
-    const {email,password}=req.body;
-    try{
-        const user=await UserModel.find({email});
-        if(user.length>0){
-            const hash=user[0].password;
-            bcrypt.compare(password,hash,(err,result)=>{
-                if(result){
-                    res.send({"msg":"Logged in","email":user[0].email,"SQ":user[0].securityQuestion});
-                }else{
-                    res.send({"msg":"Wrong Credientials"})
+userRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.find({ email });
+        if (user.length > 0) {
+            const hash = user[0].password;
+            bcrypt.compare(password, hash, (err, result) => {
+                if (result) {
+                    res.send({ "msg": "Logged in", "email": user[0].email, "SQ": user[0].securityQuestion });
+                } else {
+                    res.send({ "msg": "Wrong Credientials" })
                 }
             })
-        }else{
+        } else {
             res.send({
-                "msg":"Wrong Credientials"
+                "msg": "Wrong Credientials"
             })
         }
-    }catch(err){
-        res.send({"msg":err})
+    } catch (err) {
+        res.send({ "msg": err })
     }
 })
 
-userRouter.post("/forgetPassword",async(req,res)=>{
-    const {email,
-        password,
-        securityAnswer}=req.body;
-    try{
-        const user=await UserModel.find({email});
-        if(user.length>0){
-            if(securityAnswer==user[0].securityAnswer){
+userRouter.post("/getQuestion", async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await UserModel.find({ email });
+        if (user.length > 0) {
+            res.send({ "msg": "Security Question", "email": user[0].email, "SQ": user[0].securityQuestion });
+        }
+        else {
+            res.send({
+                "msg": "Email not found"
+            })
+        }
+    } catch (err) {
+        res.send({ "msg": err })
+    }
+})
+
+userRouter.post("/forgetPassword", async (req, res) => {
+    const { email,password,securityAnswer } = req.body;
+    try {
+        const user = await UserModel.find({ email });
+        if (user.length > 0) {
+            if (securityAnswer == user[0].securityAnswer) {
                 const hashedPassword = await bcrypt.hash(password, 5);
                 await UserModel.findOneAndUpdate(
                     { email },
                     { password: hashedPassword }
                 )
-            res.send({
-                msg: "Password changed successfully"
-            });
+                res.send({
+                    msg: "Password changed successfully"
+                });
             }
             else {
                 res.send({
@@ -75,16 +92,16 @@ userRouter.post("/forgetPassword",async(req,res)=>{
                 });
             }
         }
-        else{
+        else {
             res.send({
-                "msg":"Wrong Credentials"
+                "msg": "Wrong Credentials"
             })
         }
-    }catch(err){
-        res.send({"msg":err})
+    } catch (err) {
+        res.send({ "msg": err })
     }
 })
 
-module.exports={
+module.exports = {
     userRouter
 }
